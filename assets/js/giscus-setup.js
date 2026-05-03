0c3e2c1040052b4c385ff642005a62d470beeeb3
ly015 <liyining0712@gmail.com>
2026-05-03 18:28:48 +0000

    Deploying to gh-pages from @ ly015/ly015.github.io@009b145a273d18dde11b73f67a4ec1af3e94cc2b 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "ly015/ly015.github.io-new",
+    "data-repo": "ly015/ly015.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
